ab8b416f5eb1c05b469fd36df6d9daf5e8b5d030
Steven Cunza <71741271+steven-cunza@users.noreply.github.com>
2020-10-12 14:58:13 -0600

    Shorter JavaScript code
    Shorter functions

--- script.js
+++ script.js
@@ -1,177 +1,18 @@
 const food = document.getElementsByClassName("food-image");
-let calories = document.getElementsByClassName("calories");
+const calories = document.getElementsByClassName("calories");
 const mode = document.getElementsByClassName("mode");
 const body = document.getElementsByTagName("body");
 const header = document.getElementsByTagName("header");
 const text = document.getElementsByClassName("counter");
 
-let hamburgerCalories = parseInt(563);
-let sodaCalories = parseInt(138);
-let pizzaCalories = parseInt(285);
-let iceCreamCalories = parseInt(111);
+let caloriesCounter = [];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+let total = 0;
 
-let caloriesCounter = 0;
-
-function checkIfSelected(){
-
-    if (body[0].classList.contains("body-dark")){
-
-        if (food[0].classList.contains('food-image-dark-selected') === true &&
-            food[1].classList.contains('food-image-dark-selected') === false &&
-            food[2].classList.contains('food-image-dark-selected') === false &&
-            food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (hamburgerCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (hamburgerCalories + sodaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (hamburgerCalories + pizzaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (hamburgerCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (hamburgerCalories + sodaCalories + pizzaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (hamburgerCalories + pizzaCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (hamburgerCalories + sodaCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (sodaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (sodaCalories + pizzaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (sodaCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (sodaCalories + pizzaCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === false) {
-            caloriesCounter = (pizzaCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (pizzaCalories + iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === false &&
-                    food[1].classList.contains('food-image-dark-selected') === false &&
-                    food[2].classList.contains('food-image-dark-selected') === false &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-            caloriesCounter = (iceCreamCalories);
-        } else if (food[0].classList.contains('food-image-dark-selected') === true &&
-                    food[1].classList.contains('food-image-dark-selected') === true &&
-                    food[2].classList.contains('food-image-dark-selected') === true &&
-                    food[3].classList.contains('food-image-dark-selected') === true) {
-                caloriesCounter = (hamburgerCalories + sodaCalories + pizzaCalories + iceCreamCalories);
-        }
-    } else {
-
-        if (food[0].classList.contains('food-image-selected') === true &&
-        food[1].classList.contains('food-image-selected') === false &&
-        food[2].classList.contains('food-image-selected') === false &&
-        food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (hamburgerCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (hamburgerCalories + sodaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (hamburgerCalories + pizzaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (hamburgerCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (hamburgerCalories + sodaCalories + pizzaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (hamburgerCalories + pizzaCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (hamburgerCalories + sodaCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (sodaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (sodaCalories + pizzaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (sodaCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (sodaCalories + pizzaCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === false) {
-        caloriesCounter = (pizzaCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (pizzaCalories + iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === false &&
-                food[1].classList.contains('food-image-selected') === false &&
-                food[2].classList.contains('food-image-selected') === false &&
-                food[3].classList.contains('food-image-selected') === true) {
-        caloriesCounter = (iceCreamCalories);
-    } else if (food[0].classList.contains('food-image-selected') === true &&
-                food[1].classList.contains('food-image-selected') === true &&
-                food[2].classList.contains('food-image-selected') === true &&
-                food[3].classList.contains('food-image-selected') === true) {
-            caloriesCounter = (hamburgerCalories + sodaCalories + pizzaCalories + iceCreamCalories);
-    }
-    }
-}
+const hamburger = food[0].getAttribute("data-hamburger"); //el = element
+const soda = food[1].getAttribute("data-soda");
+const pizza= food[2].getAttribute("data-pizza");
+const iceCream = food[3].getAttribute("data-ice-cream");
 
 function darkLightMode() {
     body[0].classList.toggle("body-dark");
@@ -198,58 +39,86 @@ function darkLightMode() {
       }
 }
 
-/*
-for (i = 0; i < food.length; i++) {
-    food[i].addEventListener("click", function (e, i) {
-        e.preventDefault();
-
-        for (i = 0; i < food.length; i++) {
-        food[i].classList.toggle("food-image-selected");
-        }
-    })    
-}
-*/
+function sumCalories() {
+    total = caloriesCounter.reduce(reducer);
+} 
 
 mode[0].addEventListener("click", function (e) {
     darkLightMode();
 })
 
 food[0].addEventListener("click", function (e) {
+    
     if (body[0].classList.contains("body-dark")) {
         food[0].classList.toggle("food-image-dark-selected"); 
     } else {
         food[0].classList.toggle("food-image-selected"); 
     }        
-    checkIfSelected();
-    calories[0].innerText = caloriesCounter;
+    
+    if (food[0].classList.contains("food-image-dark-selected")) {
+        caloriesCounter[0] = parseInt(hamburger);
+    } else if (food[0].classList.contains("food-image-selected")) {
+        caloriesCounter[0] = parseInt(hamburger);
+    } else {
+        caloriesCounter[0] = parseInt(0);
+    }
+    sumCalories();
+    calories[0].innerText = total;
 })
 
 food[1].addEventListener("click", function (e) {
+    
     if (body[0].classList.contains("body-dark")) {
         food[1].classList.toggle("food-image-dark-selected"); 
     } else {
         food[1].classList.toggle("food-image-selected"); 
+    }        
+    
+    if (food[1].classList.contains("food-image-dark-selected")) {
+        caloriesCounter[1] = parseInt(soda);
+    } else if (food[1].classList.contains("food-image-selected")) {
+        caloriesCounter[1] = parseInt(soda);
+    } else {
+        caloriesCounter[1] = parseInt(0);
     }
-    checkIfSelected();
-    calories[0].innerText = caloriesCounter;
+    sumCalories();
+    calories[0].innerText = total;
 })
 
 food[2].addEventListener("click", function (e) {
+    
     if (body[0].classList.contains("body-dark")) {
         food[2].classList.toggle("food-image-dark-selected"); 
     } else {
         food[2].classList.toggle("food-image-selected"); 
+    }        
+    
+    if (food[2].classList.contains("food-image-dark-selected")) {
+        caloriesCounter[2] = parseInt(pizza);
+    } else if (food[2].classList.contains("food-image-selected")) {
+        caloriesCounter[2] = parseInt(pizza);
+    } else {
+        caloriesCounter[2] = parseInt(0);
     }
-    checkIfSelected();
-    calories[0].innerText = caloriesCounter;
+    sumCalories();
+    calories[0].innerText = total;
 })
 
 food[3].addEventListener("click", function (e) {
+    
     if (body[0].classList.contains("body-dark")) {
         food[3].classList.toggle("food-image-dark-selected"); 
     } else {
         food[3].classList.toggle("food-image-selected"); 
+    }        
+    
+    if (food[3].classList.contains("food-image-dark-selected")) {
+        caloriesCounter[3] = parseInt(iceCream);
+    } else if (food[3].classList.contains("food-image-selected")) {
+        caloriesCounter[3] = parseInt(iceCream);
+    } else {
+        caloriesCounter[3] = parseInt(0);
     }
-    checkIfSelected();
-    calories[0].innerText = caloriesCounter;
+    sumCalories();
+    calories[0].innerText = total;
 })
